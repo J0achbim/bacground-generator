42693a4a795a6b0bf2411aa678941c5a1f9db76e
J0achbim <abimbolajoachim@gmail.com>
2023-03-05 16:19:18 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,20 @@
+var database = [
+	{
+		username: "Joachim",
+		password: "abigail"
+	}
+];
+
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooo cool!"
+	}
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
